fa70e2f6b8684c2d23c1688a281d53d0e1ac4f7f
nwactor <nwactor@brandeis.edu>
2018-01-29 14:15:45 -0800

    added preventDefault to the on click function for search

--- logic.js
+++ logic.js
@@ -1,7 +1,9 @@
 
-$("#search").on("click", function(){
+$("#search").on("click", function(event){
 	let topic = $("#search-bar").val();
 	console.log(topic);
+
+	event.preventDefault();
 
 	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=3eea9854e78749d48eee727b93b5c103&q=" + topic;
 	$.ajax({
